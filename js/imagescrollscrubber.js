670076888cbab235cd2b6dc5f60fa6dd9265f224
Tim Filler <tim.filler@elricco.net>
2021-07-02 16:53:37 +0200

    working to change images based on ratio

--- js/imagescrollscrubber.js
+++ js/imagescrollscrubber.js
@@ -11,29 +11,46 @@ export function imageScrollScrubber() {
         [].slice.call(imageAnimations).map(function(el) {
             // canvas context in 2d because images
             const context = el.getContext('2d');
-            // needs to be undefined or animation will already start animating while not in view
-            let hitPoint;
+            // canvas fillStyle
+            context.fillStyle = el.dataset.fillStyle ? el.dataset.fillStyle : '#FFFFFF';
             // playbackConst determined by experimentation
             const playbackConst = 33;
             // .height-container closest (up the dom) to element (canvas)
             const heightContainer = el.closest('.height-container');
             // frameCount set as data attribute at element (canvas)
             const frameCount = el.dataset.framecount;
+            // initial ratios
+            let intitalRatio = window.innerWidth / window.innerHeight;
+            let ratio = window.innerWidth / window.innerHeight;
+
+            // needs to be undefined or animation will already start animating while not in view
+            let hitPoint;
             // first image set as data attribute at element (canvas)
-            const image = el.dataset.desktopImage;
+            let image = el.dataset.desktopImage;
+            // if ratio is <= 1 use the 9x16 version of the animation images
+            if (ratio <= 1) {
+                image = el.dataset.mobileImage;
+            }
 
-            const firstImage = d.createElement('img');
-            firstImage.classList.add('d-none');
+            const drawFirstImageForDimensions = () => {
+                // create image element in document - is needed to get the actual dimensions
+                const firstImage = d.createElement('img');
 
-            firstImage.onload = function() {
-                console.log(this.width + ' x ' + this.height);
-                el.width = this.width;
-                el.height = this.height;
+                // onload event
+                firstImage.onload = function() {
+                    el.width = this.width;
+                    el.height = this.height;
+                };
+
+                // load image
+                firstImage.src = image;
             };
 
-            firstImage.src = image;
+            // draw immediately to set dimension as soon as possible
+            drawFirstImageForDimensions();
+
             // deconstruct string of image by last underscore _ filenames have to be name-xyz_0000.jpg,  name-xyz_0001.jpg, etc.
-            const imageString = image.substr(0, image.lastIndexOf('_'));
+            let imageString = image.substr(0, image.lastIndexOf('_'));
 
             // set heigt of wrapping container for enought stickyness and scrolling
             heightContainer.style.height = Math.ceil((frameCount * playbackConst)) + 'px';
@@ -100,6 +117,49 @@ export function imageScrollScrubber() {
                 // repeat image update process
                 requestAnimationFrame(() => updateImage(frameIndex + 1));
             };
+
+            // debug stuff
+            const heightOutput = document.querySelector('#height');
+            const widthOutput = document.querySelector('#width');
+            const ratioOutput = document.querySelector('#ratio');
+
+            const reportWindowSize = () => {
+                // get actual ratio here again
+                ratio = window.innerWidth / window.innerHeight;
+
+                // debug stuff
+                heightOutput.textContent = window.innerHeight;
+                widthOutput.textContent = window.innerWidth;
+                ratioOutput.textContent = ratio;
+
+                // only redraw everything on change of ratios
+                if (ratio <= 1 && intitalRatio >= 1) {
+                    // get different image set
+                    image = el.dataset.mobileImage;
+                    // decontruct string agoin to load right images
+                    imageString = image.substr(0, image.lastIndexOf('_'));
+                    // draw the first image to set new dimensions of element (canvas)
+                    drawFirstImageForDimensions();
+                    // start with preloading the images of the element (canvas)
+                    preloadImages();
+                    // set intitalRatio to actual ratio to only redraw on turnign point (1)
+                    intitalRatio = ratio;
+                } else if (ratio >= 1 && intitalRatio <= 1) {
+                    // get different image set
+                    image = el.dataset.desktopImage;
+                    // decontruct string agoin to load right images
+                    imageString = image.substr(0, image.lastIndexOf('_'));
+                    // draw the first image to set new dimensions of element (canvas)
+                    drawFirstImageForDimensions();
+                    // start with preloading the images of the element (canvas)
+                    preloadImages();
+                    // set intitalRatio to actual ratio to only redraw on turnign point (1)
+                    intitalRatio = ratio;
+                }
+            };
+
+            // add event listener for resize event
+            window.addEventListener('resize', reportWindowSize);
 
             // start with preloading the images of the element (canvas)
             preloadImages();
